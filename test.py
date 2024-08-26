10d069ba6095b53a2f6beb44245c756e41bdc60b
cole sashkin <csashceh1024@gmail.com>
2024-08-25 17:57:34 -0700

    update

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from utils import play_wavfile
 import os
+import numpy as np
 from scipy.io import wavfile
 import scipy.signal as sps
 
@@ -12,5 +13,17 @@ def load_wav_16k_mono(fname):
     return wav
 
 fname = os.path.join(os.getcwd(), "rgb_wavs", "rgb", "red", "red5.wav")
-wav = load_wav_16k_mono(fname)
-play_wavfile(fname)
+# wav = load_wav_16k_mono(fname)
+# play_wavfile(fname)
+
+# audio read
+origin_sample_rate, origin_audio = wavfile.read(fname)
+wavfile.write('red_orig.wav', origin_sample_rate, origin_audio[:,0])
+
+origin_num_samples, origin_num_channels = origin_audio.shape
+
+new_samps = int(origin_num_samples * 16000/44100)
+
+# resampling
+target_audio_scipy = sps.resample(origin_audio[:,0], new_samps).astype(int)
+target_audio_scipy = np.array(target_audio_scipy, np.int16)
