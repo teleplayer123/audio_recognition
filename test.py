826313a07654d6184b7842a6d342a100a7cf8ec4
Cole Sashkin <csashceh1024@gmail.com>
2024-08-30 11:45:00 -0700

    update

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import os
+from utils import convert_saved_model_to_tflite
 
 
 input_shape = (124, 129, 1)
@@ -8,3 +9,4 @@ save_dir = os.path.join(os.getcwd(), "models")
 model_dir = os.path.join(save_dir, "saved_model")
 label_path = os.path.join(save_dir, "labels.txt")
 
+convert_saved_model_to_tflite(model_dir)
