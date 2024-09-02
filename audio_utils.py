1b0c6f6c42c2370bb9ae3aeaf76920c34644d33b
cole sashkin <csashceh1024@gmail.com>
2024-09-02 10:00:52 -0700

    update

--- audio_utils.py
+++ audio_utils.py
@@ -1,7 +1,8 @@
 import numpy as np
 from scipy.io import wavfile
-from scipy.signal import resample
-import random
+import scipy.signal as sps
+import tensorflow as tf
+import os
 
 
 def downsample_waveform(waveform, num_bins):
@@ -15,42 +16,24 @@ def downsample_waveform(waveform, num_bins):
         downsampled_waveform[i] = waveform[start_index:end_index].mean()
     return downsampled_waveform.tolist()
 
+def add_white_noise(audio):
+    #generate noise and the scalar multiplier
+    noise = tf.random.uniform(shape=tf.shape(audio), minval=-1, maxval=1)
+    noise_scalar = tf.random.uniform(shape=[1], minval=0, maxval=0.2)
+    # add them to the original audio
+    audio_with_noise = audio + (noise * noise_scalar)
+    # final clip the values to ensure they are still between -1 and 1
+    audio_with_noise = tf.clip_by_value(audio_with_noise, clip_value_min=-1, clip_value_max=1)
+    return audio_with_noise
 
-def extract_features(audio_file_path, noise_file_path, window_size, overlap,add_noise=True, sample_rate=8192, noise_ampl=0.20, num_bins=16):
-    # noise_file_path = "/kaggle/input/google-speech-commands/_background_noise_/white_noise.wav"
-    target_sample_rate = sample_rate
-
-    # Read white noise audio file
-    noise_sample_rate, noise_data = wavfile.read(noise_file_path)
-    noise_duration = len(noise_data) / noise_sample_rate
-    
-    # Generate random start time within the noise file
-    random_start_time = random.uniform(0, noise_duration - 1)
-    random_end_time = random_start_time + 1  # 1-second segment
-
-    # Extract random 1-second segment of white noise
-    start_index = int(random_start_time * noise_sample_rate)
-    end_index = int(random_end_time * noise_sample_rate)
-    random_noise_segment = noise_data[start_index:end_index]
-
-    # Resample white noise segment to target sample rate
-    resampled_noise_segment = resample(random_noise_segment, target_sample_rate)
-    if add_noise:
-        resampled_noise_segment = noise_ampl * resampled_noise_segment
-    else:
-        resampled_noise_segment = 0 * resampled_noise_segment
-
-    # Read and resample audio file
+def extract_features(audio_file_path, window_size=1024, overlap=0, num_bins=16):
     sample_rate, audio_data = wavfile.read(audio_file_path)
-    resampled_audio = resample(audio_data, target_sample_rate)
-
+    resampled_audio = sps.resample(audio_data, sample_rate)
     # Add white noise to the audio
-    augmented_audio = resampled_audio + resampled_noise_segment
-
+    augmented_audio = add_white_noise(resampled_audio)
     step_size = window_size - overlap
     num_windows = (len(augmented_audio) - window_size) // step_size + 1
     fft_results = []
-
     for i in range(num_windows):
         start_index = i * step_size
         end_index = start_index + window_size
@@ -62,5 +45,20 @@ def extract_features(audio_file_path, noise_file_path, window_size, overlap,add_
         fft_magnitude[0] = 0
         fft_magnitude = downsample_waveform(fft_magnitude, num_bins)
         fft_results.extend(fft_magnitude)
-
     return np.array(fft_results)
+
+def load_data(data_dir):
+    waveforms = []
+    labels = []
+    for dirname in os.listdir(data_dir):
+        label_dir = os.path.join(data_dir, dirname)
+        if not dirname in labels:
+            labels.append(dirname)
+        wav_files = [os.path.join(label_dir, fname) for fname in os.listdir(label_dir)]
+        feature_arr = []
+        for wav_file in wav_files:
+            xfeatures = extract_features(wav_file)
+            feature_arr.append(xfeatures)
+        waveforms.append(np.array(feature_arr))
+        del feature_arr
+    return np.array(waveforms), np.array(labels)
