524182ba7aaf228f4bc1cbabfdf3d50b8158a53c
Cole Sashkin <csashceh1024@gmail.com>
2024-09-06 12:21:25 -0700

    update

--- feather_test.py
+++ feather_test.py
@@ -1,12 +1,15 @@
 import time
 import array
-import math
 import board
 import audiobusio
 import neopixel
 from ulab import numpy as np
 import ulab
 import gc
+import svm_red
+import svm_green
+import svm_blue
+
 
 
 # Helper functions to remove DC bias and compute RMS
@@ -16,39 +19,39 @@ def mean(values):
 def normalized_rms(values):
     minbuf = int(mean(values))
     samples_sum = sum(float(sample - minbuf) * (sample - minbuf) for sample in values)
-    return math.sqrt(samples_sum / len(values))
+    return np.sqrt(samples_sum / len(values))
 
 def read_audio_data(mic):
     n_samples = 8192
     data = array.array("H", [0] * n_samples)
     mic.record(data, n_samples)
-    return data
+    return np.array(data)
 
 def get_spectrogram(data):
     spect = ulab.utils.spectrogram(data)
     spect[0] = 0
     return spect
 
-def avg_spectrogram(data, n_bins):
-    n_chunks = len(data) // n_bins
-    res = [np.mean(data[i*n_bins:(i+1)*n_bins]) for i in range(n_chunks)]
-    return res
+def downsample_waveform(waveform, n_bins):
+    waveforms = np.zeros(n_bins)
+    waveform = np.array(waveform)
+    n_points = len(waveform) // n_bins
+    for i in range(n_bins):
+        start = i * n_points
+        end = start + n_points
+        waveforms[i] = np.mean(waveform[start:end])
+    return waveforms
 
 def convert_spectrogram(data):
-    n_bins = 32
-    fft_size = 64
+    orig_len = 8192 // 32
+    n_bins = 16
+    fft_size = 32
     res = []
-    for i in range(0, len(data), fft_size):
-        start = i * fft_size
-        end = start + fft_size
-        chunk = data[start:end]
-        if len(chunk) != fft_size:
-            continue
-        spec = get_spectrogram(chunk[:len(chunk)//2])
-        mspec = avg_spectrogram(spec, n_bins)
+    for i in range(0, orig_len, fft_size):
+        spec = get_spectrogram(data[i*fft_size:i*fft_size+fft_size])
+        mspec = downsample_waveform(spec, n_bins)
         res.extend(mspec)
-    res = normalized_rms(np.array(res))
-    return res
+    return np.array(res)
 
 def set_color(led, color):
     if color.lower() == "red":
@@ -66,11 +69,20 @@ led.brightness = 0.3
 
 #setup mic
 mic = audiobusio.PDMIn(board.TX, board.D12, sample_rate=16000, bit_depth=16)
-i = 0
+
 while True:
     data = read_audio_data(mic)
-    print(i)
-    if sum(data) > 1:
-        break
-    i+=1
-print(data)
+    if len(data) >= 8192:
+        print(len(data))
+        spec = convert_spectrogram(data)
+        print(len(spec))
+        rscore = svm_red.score(spec)
+        print("Red Score: {}".format(rscore))
+        gscore = svm_green.score(spec)
+        print("Green Score: {}".format(gscore))
+        bscore = svm_blue.score(spec)
+        print("Blue Score: {}".format(bscore))
+        del data
+        del spec
+        gc.collect()
+
