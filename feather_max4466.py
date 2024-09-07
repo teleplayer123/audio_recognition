60bd847af823f99082e443fdc5264b3a1104956a
cole sashkin <csashceh1024@gmail.com>
2024-09-06 23:02:21 -0700

    update

---
+++ feather_max4466.py
@@ -0,0 +1,86 @@
+import time
+import board
+import analogio
+import neopixel
+from ulab import numpy as np
+import ulab
+import gc
+import svm_red
+import svm_green
+import svm_blue
+
+
+def read_audio_data(mic):
+    n_samples = 1024 
+    data = []
+    for i in range(n_samples):
+        data.append(100*((mic.value * 3.3 / 65536) - 1.65))
+    return np.array(data)
+
+
+def downsample_waveform(waveform, n_bins):
+    waveforms = np.zeros(n_bins)
+    n_points = len(waveform) // n_bins
+    for i in range(n_bins):
+        start = i * n_points
+        end = start + n_points
+        waveforms[i] = np.mean(waveform[start:end])
+    return waveforms
+
+def convert_spectrogram(data):
+    n_bins = 32
+    fft_size = 64
+    res = []
+    for i in range(0, len(data), fft_size):
+        spec = ulab.utils.spectrogram(data[i:i+fft_size])
+        spec[0] = 0
+        mspec = downsample_waveform(spec, n_bins)
+        res.extend(mspec)
+        del spec
+        del mspec
+        gc.collect()
+    res = np.array(res)
+    amax = np.argmax(res)
+    amin = np.argmin(res)
+    nres = (res - amin) / (amax - amin)
+    del amax
+    del amin
+    del res
+    gc.collect()
+    return np.array(nres)
+
+def set_color(led, color):
+    if color.lower() == "red":
+        led[0] = (255, 0, 0)
+    elif color.lower() == "green":
+        led[0] = (0, 255, 0)
+    elif color.lower() == "blue":
+        led[0] = (0, 0, 255)
+    else:
+        led[0] = (0, 0, 0)
+
+#setup led
+led = neopixel.NeoPixel(board.NEOPIXEL, 1)
+led.brightness = 0.3
+
+#setup mic
+mic = analogio.AnalogIn(board.A2)
+
+while True:
+    data = read_audio_data(mic)
+    if len(data) > 1:
+        spec = convert_spectrogram(data)
+        print(len(spec))
+        score = svm_red.score(spec)
+        print("Red Score: {}".format(score))
+        score = svm_green.score(spec)
+        print("Green Score: {}".format(score))
+        score = svm_blue.score(spec)
+        print("Blue Score: {}".format(score))
+        del score
+        del data
+        del spec
+        gc.collect()
+        time.sleep(1)
+
+
