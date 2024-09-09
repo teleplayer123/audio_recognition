ab925739c2ec15ce939ca59a0407194ad8e1fc35
Cole Sashkin <csashceh1024@gmail.com>
2024-09-09 10:10:09 -0700

    update

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-from lib.red_model import score
+from models.models128_lib.red_model import score
 import numpy as np
 import os
 from audio_processing import extract_features
 
 red0 = os.path.join(os.getcwd(), "rgb_wavs", "rgb", "red", "red0.wav")
-arr = extract_features(red0)
-s = score(arr)
+arr = extract_features(red0, target_sample_rate=8192)
+s = score(arr)[0]
 print("score: {}".format(s))
