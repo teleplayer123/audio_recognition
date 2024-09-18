36d2c83ff72212287c1f23b32d512bdd72bf4fbb
cole sashkin <csashceh1024@gmail.com>
2024-09-18 00:18:23 -0700

    update

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import wave
 from scipy.io import wavfile
 import scipy.signal as sps
 import tensorflow as tf
+import tensorflow_io as tfio
 import numpy as np
 import math
 
@@ -257,16 +258,20 @@ def load_wav_mono(fname, rate_out=16000):
     wav = sps.resample(data, rate_out)
     return wav
 
-def load_wav_16k_mono(filename, rate_out=16000):
-    """ Load a WAV file, convert it to a float tensor, resample to 16 kHz single-channel audio. """
+@tf.function
+def load_wav_16k_mono(filename):
+    """ Load a WAV file, convert it to a float tensor, resample to 16 kHz single-channel audio. 
+        !pip install -q "tensorflow==2.11.*"
+        # tensorflow_io 0.28 is compatible with TensorFlow 2.11
+        !pip install -q "tensorflow_io==0.28.*"
+    """
     file_contents = tf.io.read_file(filename)
     wav, sample_rate = tf.audio.decode_wav(
           file_contents,
           desired_channels=1)
     wav = tf.squeeze(wav, axis=-1)
     sample_rate = tf.cast(sample_rate, dtype=tf.int64)
-    wav = sps.resample(wav, num=rate_out)
-    wav = tf.convert_to_tensor(wav, dtype=tf.int64)
+    wav = tfio.audio.resample(wav, rate_in=sample_rate, rate_out=16000)
     return wav
 
 def load_data_rgb_multi_class(data_dir):
